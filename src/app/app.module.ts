d1b9994043276daed163cadf56c07a0e7d010747
jihenJN <jihenjihen23@gmail.com>
2023-07-13 00:23:26 +0100

    remove useless imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,63 +8,10 @@ import { TableFilterRowDemo } from 'src/app/demo/table-filter-row-demo';
 
 
 // Import PrimeNG modules
-import { AccordionModule } from 'primeng/accordion';
-import { AutoCompleteModule } from 'primeng/autocomplete';
 import { AvatarModule } from 'primeng/avatar';
-import { BadgeModule } from 'primeng/badge';
-import { BreadcrumbModule } from 'primeng/breadcrumb';
-import { ButtonModule } from 'primeng/button';
-import { CalendarModule } from 'primeng/calendar';
-import { CarouselModule } from 'primeng/carousel';
-import { CascadeSelectModule } from 'primeng/cascadeselect';
-import { ChartModule } from 'primeng/chart';
-import { CheckboxModule } from 'primeng/checkbox';
-import { ChipModule } from 'primeng/chip';
-import { ChipsModule } from 'primeng/chips';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { ColorPickerModule } from 'primeng/colorpicker';
-import { ContextMenuModule } from 'primeng/contextmenu';
-import { DataViewModule } from 'primeng/dataview';
-import { VirtualScrollerModule } from 'primeng/virtualscroller';
-import { DialogModule } from 'primeng/dialog';
-import { DividerModule } from 'primeng/divider';
-import { DockModule } from 'primeng/dock';
-import { DragDropModule } from 'primeng/dragdrop';
 import { DropdownModule } from 'primeng/dropdown';
-import { DynamicDialogModule } from 'primeng/dynamicdialog';
-import { EditorModule } from 'primeng/editor';
-import { FieldsetModule } from 'primeng/fieldset';
-import { FileUploadModule } from 'primeng/fileupload';
-import { GalleriaModule } from 'primeng/galleria';
-import { InplaceModule } from 'primeng/inplace';
-import { InputMaskModule } from 'primeng/inputmask';
-import { InputSwitchModule } from 'primeng/inputswitch';
 import { InputTextModule } from 'primeng/inputtext';
-import { InputNumberModule } from 'primeng/inputnumber';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { ImageModule } from 'primeng/image';
-import { KnobModule } from 'primeng/knob';
-import { ListboxModule } from 'primeng/listbox';
-import { MegaMenuModule } from 'primeng/megamenu';
-import { MenuModule } from 'primeng/menu';
-import { MenubarModule } from 'primeng/menubar';
-import { MessageModule } from 'primeng/message';
-import { MessagesModule } from 'primeng/messages';
 import { MultiSelectModule } from 'primeng/multiselect';
-import { OrderListModule } from 'primeng/orderlist';
-import { OverlayPanelModule } from 'primeng/overlaypanel';
-import { PaginatorModule } from 'primeng/paginator';
-import { PanelModule } from 'primeng/panel';
-import { PanelMenuModule } from 'primeng/panelmenu';
-import { PickListModule } from 'primeng/picklist';
-import { ProgressBarModule } from 'primeng/progressbar';
-import { RadioButtonModule } from 'primeng/radiobutton';
-import { RatingModule } from 'primeng/rating';
-import { ScrollerModule } from 'primeng/scroller';
-import { ScrollPanelModule } from 'primeng/scrollpanel';
-import { ScrollTopModule } from 'primeng/scrolltop';
-import { SelectButtonModule } from 'primeng/selectbutton';
-import { SidebarModule } from 'primeng/sidebar';
 
 import { TableModule } from 'primeng/table';
 
@@ -72,7 +19,6 @@ import { TagModule } from 'primeng/tag';
 
 import { AnimateModule } from 'primeng/animate';
 
-import { BlockUIModule } from 'primeng/blockui';
 
 import { CustomerService } from 'src/service/customerservice';
 
@@ -87,49 +33,8 @@ import { CustomerService } from 'src/service/customerservice';
     TagModule,
     TableModule,
     MultiSelectModule,
-
-    ConfirmDialogModule,
-    ContextMenuModule,
-    VirtualScrollerModule,
-    DataViewModule,
-    DialogModule,
-    DividerModule,
-    DockModule,
-    DragDropModule,
-    DropdownModule,
-    DynamicDialogModule,
-    EditorModule,
-    FieldsetModule,
-    
-    InputMaskModule,
-    InputSwitchModule,
     InputTextModule,
-    InputTextareaModule,
-    InputNumberModule,
-   
-   
-    
-   
-    
-    
-   
-   
-   
-    
-   
-    
-  
-    
-    
-  
-   
-    
-   
-  
-   
-   
-   
-   
+    DropdownModule,
     AnimateModule,
   
     RouterModule.forRoot([{ path: '', component: TableFilterRowDemo }])],
